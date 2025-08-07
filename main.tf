54a04c261c6205f3d66eee6a89ef1ab2ebce5305
vyshnavvijayan1131 <vyshnavvijayan2003@gmail.com>
2025-08-07 11:33:25 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-container"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
